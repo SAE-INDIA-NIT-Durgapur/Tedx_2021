d4033971dcc1210589bc6f3e1d22c05b59e35f2e
Simpola <74448357+Techno-Simpola@users.noreply.github.com>
2021-09-01 13:47:04 +0530

    Reg page customization

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,14 +4,18 @@ import logo from './tedxnitdgp.png'
 
 const Register = () => {
     return (
-        <div>
-            <div className="register">
+           
+         <div className="fluid-container register-main">
+            <div className="row container register">
                 <img className="image_register" src={logo}/>
-                <p className="Coming_Soon"> 
+                <p 
+                className="Coming_Soon"> 
                     Registrations<br /> Coming soon
                 </p>
+                
             </div>
         </div>
+
     )
 }
 
