b31912c93b0ab0a33c56c103f38cbef7d25b7b31
SubhamGupta007 <subhamyoyo23@gmail.com>
2021-09-19 20:58:35 +0530

    minor chnages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,39 +2,36 @@ import React from 'react'
 import * as THREE from 'three';
 import Slide from 'react-reveal/Slide';
 import { useState, useEffect, useRef } from 'react'
-// import WAVES from '../../node_modules/vanta/dist/vanta.net.min'
+import BIRDS from '../../node_modules/vanta/dist/vanta.waves.min'
 import Countdown from "./Countdown"
 import logo from "../images/tedxnitdgp.png"
 
 
 const Home = () => {
-    // const [vantaEffect, setVantaEffect] = useState(0)
-    // const myRef = useRef(null)
-    // useEffect(() => {
-    //     if (!vantaEffect) {
-    //         window.THREE = THREE;
-    //         setVantaEffect(WAVES({
-    //             el: myRef.current,
-    //             mouseControls: true,
-    //             touchControls: true,
-    //             gyroControls: false,
-    //             minHeight: 200.00,
-    //             minWidth: 200.00,
-    //             scale: 1.00,
-    //             scaleMobile: 1.00,
-    //             color: 0xff2b06,
-    //             backgroundColor: 0x1b1b1b,
-    //             THREE: window.THREE
-    //         }));
-    //     }
-    //     return () => {
-    //         if (vantaEffect) vantaEffect.destroy()
-    //     }
-    // }, [vantaEffect])
+   const [vantaEffect, setVantaEffect] = useState(0)
+    const myRef = useRef(null)
+    useEffect(() => {
+        if (!vantaEffect) {
+            setVantaEffect(BIRDS({
+                el: myRef.current,
+                mouseControls: true,
+                touchControls: true,
+                gyroControls: false,
+                minHeight: 200.00,
+                minWidth: 200.00,
+                scale: 1.00,
+                scaleMobile: 1.00,
+                color: 0x0
+            }))
+        }
+        return () => {
+            if (vantaEffect) vantaEffect.destroy()
+        }
+    }, [vantaEffect])
 
 
     return (
-        <div className="top-main">
+        <div className="top-main" ref={myRef}>
             <section className="home ">
                 <div className="container-fluid justify-content-center align-items-center">
                     <div className="row  vh-100  justify-content-center align-items-center" >
