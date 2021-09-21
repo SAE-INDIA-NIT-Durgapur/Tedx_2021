39cd15749b41a97dad860153c3be65e8ae56c266
Techno-Simpola <simpolasuristus@gmail.com>
2021-09-21 22:58:52 +0530

    added team  members

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,8 +5,9 @@ import Slide from 'react-reveal/Slide';
 const TeamView = () => {
    return (
        <>
-       <h1 className="text-center" style={{color:"#FF2B06", paddingTop:"50px"}}>Our Team</h1>
-      
+       <h1 className="text-center heading-team" style={{color:"#FF2B06", paddingTop:"50px"}}>OUR TEAM
+       <hr/></h1>
+      <h1 className="text-center heading-team" style={{color:"#FF2B06", paddingTop:"50px", fontFamily:"Montserrat"}}>ORGANIZERS</h1>
        <div className="fluid-container container-team">
       
       <Slide to bottom>
@@ -19,7 +20,7 @@ const TeamView = () => {
         </div>
         <div className="content">
           <div className="contentBx">
-            <h3>ASHUTOSH <br /><span>Web Developer</span></h3>
+            <h3>Chetan Gupta <br /><span>Licensee</span></h3>
           </div>
           <ul className="sci">
             <li>
@@ -40,7 +41,7 @@ const TeamView = () => {
         </div>
         <div className="content">
           <div className="contentBx">
-            <h3>ISHAN <br /><span>Web Developer</span></h3>
+            <h3>Shubham Agarwal <br /><span>Co-Organiser</span></h3>
           </div>
           <ul className="sci">
             <li>
@@ -61,7 +62,7 @@ const TeamView = () => {
         </div>
         <div className="content">
           <div className="contentBx">
-            <h3>ASIF <br /><span>Web Developer</span></h3>
+            <h3>V Vishesh <br /><span>Cheif Curator</span></h3>
           </div>
           <ul className="sci">
             <li>
@@ -72,7 +73,7 @@ const TeamView = () => {
       </div>
       </Slide>
 
-        <Slide to bottom>
+      <Slide to bottom>
        <div className="card">
         <div className="imgBx">
           <img
@@ -82,7 +83,346 @@ const TeamView = () => {
         </div>
         <div className="content">
           <div className="contentBx">
-            <h3>KUSHAL <br /><span>Web Developer</span></h3>
+            <h3>Rounak Das <br /><span>Executive Producer</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+      
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Shubhangee <br /><span>Director of Operations</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+      <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Ambika Biswas Neela <br /><span>Assistant Director of Operations</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+      <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Ayush Abhishek Kujur  <br /><span>Director of Public Relation</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+      <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Director of Finance <br /><span>Soumik Hazra</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Amit Tiwari <br /><span>Associate Director of Finance</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+      
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Arunava Sarkar <br /><span>Director of Communication & Marketing</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Abhishek Singh <br /><span>Associate Director of Communication & Marketing</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Kumar Gaurav <br /><span>Webmaster</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Jissu <br /><span>Video & Production Lead</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Samridho Saha <br /><span>cheif Designer</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+    </div>
+    <h1 className="text-center heading-team" style={{color:"#FF2B06", paddingTop:"50px", fontFamily:"Montserrat"}}>EXECUTIVES<hr/></h1>
+    <div className="fluid-container container-team">
+      
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Divas <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Satyam <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Shubham <br /><span>Web Developer</span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Anurag <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Hemanth <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Aniket <br /><span></span></h3>
           </div>
           <ul className="sci">
             <li>
@@ -103,7 +443,7 @@ const TeamView = () => {
         </div>
         <div className="content">
           <div className="contentBx">
-            <h3>RAHUL <br /><span>Web Developer</span></h3>
+            <h3>Sukanya <br /><span></span></h3>
           </div>
           <ul className="sci">
             <li>
@@ -113,8 +453,197 @@ const TeamView = () => {
         </div>   
       </div>
       </Slide>
-    </div>
-       </>
+
+       <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Suryadip <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+       <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Anshika <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+       <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Vishal <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+       <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Sivaji <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+       <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Ayush tiwari <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+       <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Tilakraj <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+       <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Anwesha Dan <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Sammanita <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+
+      <Slide to bottom>
+       <div className="card">
+        <div className="imgBx">
+          <img
+            src="https://images.pexels.com/photos/3379933/pexels-photo-3379933.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            alt=""
+          />
+        </div>
+        <div className="content">
+          <div className="contentBx">
+            <h3>Dyuti <br /><span></span></h3>
+          </div>
+          <ul className="sci">
+            <li>
+              <a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+      </Slide>
+      </div>
+    </>
    )
 }
 
