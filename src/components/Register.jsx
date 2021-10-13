4ae4866029ddfc843aca93bc7a218d7ea3fe6ceb
Sakshi <sakshi.051717@gmail.com>
2021-10-13 17:31:31 +0530

    pTable + form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import "../CSS/register.css"
 import logo from './tedxnitdgp.png'
 import Form from './Form.jsx'
+import PTable from './pTable'
 
 const Register = () => {
     return (
-           
          <div className="fluid-container register-main">
+                       
             <div className="row container register">
                 {/* <img className="image_register" src={logo}/> */}
                 <br/><br/><br/><br/>
@@ -14,6 +15,7 @@ const Register = () => {
                 className="Coming_Soon"> 
                     Registrations<br /> Coming soon
                 </p> */}
+                 <PTable/>
                 <Form />
                 
             </div>
---
+++ src/components/pTable.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../CSS/register.css";
+
+const PTable = () => {
+  return (
+      <div className="table">
+          <h1 style={{marginBottom:'20px',}} >Price Guide</h1>
+ <table className="table table-dark border-danger table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">Ticket</th>
+          <th scope="col">T-Shirt</th>
+          <th scope="col">Ticket + Tshirt</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>INR 99</td>
+          <td>INR 299</td>
+          <td>INR 349</td>
+        </tr>
+        
+      </tbody>
+    </table>
+      </div>
+   
+  );
+};
+
+export default PTable;
