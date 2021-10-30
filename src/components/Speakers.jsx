84189b9730158db9542762aa49d9c998b9e13f6b
Techno-Simpola <ashukumarsinghrathore123@gmail.com>
2021-10-31 00:29:52 +0530

    speakers updated

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -9,30 +9,6 @@ const Speakers = () => {
     <div className="speakers-all">
 
       <Slide to right>
-        {/* <div className="container contain">
-             <div className="col-10 ted-para" >
-              <h2 className="p1">
-                Why to choose<span className="p2">   TED</span>
-              </h2>
-              
-              <p className="mb-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
-                volutpat odio facilisis mauris sit amet massa. Quam nulla
-                porttitor massa id neque aliquam. Amet aliquam id diam maecenas
-                ultricies. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper
-                sit amet. Id nibh tortor id aliquet lectus proin nibh. Ac odio
-                tempor orci dapibus ultrices in iaculis. Ut porttitor leo a diam
-                sollicitudin tempor. adipiscing elit ut aliquam. Tristique
-                senectus et netus et malesuada fames. Consequat nisl vel pretium
-                lectus quam id leo in. Ultricies tristique nulla aliquet enim
-                tortor. Varius vel pharetra vel turpis nunc eget lorem. Mauris
-                in aliquam sem fringilla ut morbi tincidunt. Proin libero nunc
-                
-              </p>
-              
-            </div>
-               </div>  */}
       </Slide>
       <div className="fluid-container register-main">
         <div className="speaker"><h1
@@ -52,19 +28,16 @@ const Speakers = () => {
           </p>
         </div>
       </div> */}
-      {/* <p className="container ans">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Nihil ea sapiente pariatur iste dignissimos deleniti eaque
-                   aspernatur qui modi deserunt inventore temporibus officiis
-                    fugit excepturi incidunt, animi explicabo alias! Deserunt.
-                    </p> */}
+      
       <Slide to bottom>
         <div className="fluid-container">
         <div className="block">
           <div className="overlay" />
           <div className="block--text">
             <div className="text--head"></div>
-            {/* <div className="text--quote">“The best-selling author of over 30 novels and the Diva Magazine Author
-                                                 of the year award nominee for 2020 and 2021”</div> */}
+             <div className="text--quote style={{ color:white }}">“ 
+             A research intern at Harvard, Mr. Mahore’s ingenious brainchild, Oxyserve,
+              a device to monitor and optimize the use of oxygen during the COVID-19 pandemic, was selected for the CERN - Entrepreneurship Program 2021.”</div>
             <div className="text--person"></div>
           </div>
         </div>
@@ -72,8 +45,7 @@ const Speakers = () => {
           <div className="overlay" />
           <div className="block--text">
             <div className="text--head"></div>
-            {/* <div className="text--quote">“Head of the Department of Mental Health and Behavioral Sciences at Fortis Healthcare,
-                                           A clinical psychologist by profession, with an impressive reputation.”</div> */}
+            <div className="text--quote">“The best-selling author of over 30 novels and the Diva Magazine Author of the year award nominee for 2020 and 2021”</div>
             <div className="text--person"></div>
           </div>
         </div>
@@ -81,8 +53,9 @@ const Speakers = () => {
           <div className="overlay" />
           <div className="block--text">
             <div className="text--head"></div>
-            {/* <div className="text--quote">“A fine cook who integrated the dynamic Bengali spices with the seasoned western palette,
-                                           she presented the far eastern delicacies to the world on MasterChef Australia”</div> */}
+            <div className="text--quote">“
+            Head of the Department of Mental Health and Behavioral Sciences at Fortis Healthcare, A clinical psychologist by profession, with an impressive reputation.
+            ”</div>
             <div className="text--person"></div>
           </div>
         </div>
@@ -90,9 +63,8 @@ const Speakers = () => {
           <div className="overlay" />
           <div className="block--text">
             <div className="text--head"></div>
-            {/* <div className="text--quote">“A research intern at Harvard, Mr. Mahore’s ingenious brainchild, Oxyserve,
-                                                a device to monitor and optimize the use of oxygen during the COVID-19 pandemic,
-                                             was selected for the CERN - Entrepreneurship Program 2021.”</div> */}
+            <div className="text--quote">“
+            A fine cook who integrated the dynamic Bengali spices with the seasoned western palette, she presented the far eastern delicacies to the world on MasterChef Australia”</div>
             <div className="text--person"></div>
           </div>
         </div>
@@ -100,19 +72,19 @@ const Speakers = () => {
           <div className="overlay" />
           <div className="block--text">
             <div className="text--head"></div>
-            {/* <div className="text--quote">“Founder of one of the world’s fastest-growing tech company, Global Inc., became the youngest leader in the history of the World Economic Forum”</div> */}
+            <div className="text--quote">“Founder of one of the world’s fastest-growing tech company, Global Inc., became the youngest leader in the history of the World Economic Forum”</div>
             <div className="text--person"></div>
           </div>
         </div>
-        { <div className="block">
+        <div className="block">
           <div className="overlay" />
           <div className="block--text">
             <div className="text--head"></div>
-            {/*<div className="text--quote"></div>"Founder of startups MENTORx Global and HoverRobotix. He has been awarded the prestigious "Karmveer Chakra" by United Nations. He is also the Noble Asian of the Year 2018"</div>*/}
+            <div className="text--quote">"Founder of startups MENTORx Global and HoverRobotix. He has been awarded the prestigious "Karmveer Chakra" by United Nations. He is also the Noble Asian of the Year 2018"</div>
             <div className="text--person"></div>
           </div>
-        </div> }
-      </div>
+        </div>
+        </div>
       </Slide>
     </div>
 
