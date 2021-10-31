64150aab7b9ac1377c981d68d2c0ce09ab78ccbc
SubhamGupta007 <subhamyoyo23@gmail.com>
2021-10-31 09:33:45 +0530

    added tedx is live after timer ends

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -28,78 +28,81 @@ const getTimeDays = (time) => (time / daySeconds) | 0;
 
 
 function Countdown() {
-  const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
-  const endTime =  1635676200; // use UNIX timestamp in seconds
+  const startTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = 1635676200; // use UNIX timestamp in seconds
 
-  const remainingTime = endTime - stratTime;
+  const remainingTime = endTime - startTime;
   const days = Math.ceil(remainingTime / daySeconds);
   const daysDuration = days * daySeconds;
 
   return (
     <>
-      <div className="fluid-container App_countdown">
+      {
+        (remainingTime) ?
+          <div className="fluid-container App_countdown">
 
-        <div className="counter">
-          <CountdownCircleTimer
-            {...timerProps}
-            colors={[['#FFF5EE']]}
-            duration={daysDuration}
-            initialRemainingTime={remainingTime}
-          >
-            {({ elapsedTime }) =>
-              renderTime("days", getTimeDays(daysDuration - elapsedTime))
-            }
-          </CountdownCircleTimer>
-        </div>
-        <div className="counter">
-          <CountdownCircleTimer
-            {...timerProps}
-            colors={[['#FFF5EE']]}
+            <div className="counter">
+              <CountdownCircleTimer
+                {...timerProps}
+                colors={[['#FFF5EE']]}
+                duration={daysDuration}
+                initialRemainingTime={remainingTime}
+              >
+                {({ elapsedTime }) =>
+                  renderTime("days", getTimeDays(daysDuration - elapsedTime))
+                }
+              </CountdownCircleTimer>
+            </div>
+            <div className="counter">
+              <CountdownCircleTimer
+                {...timerProps}
+                colors={[['#FFF5EE']]}
 
-            duration={daySeconds}
-            initialRemainingTime={remainingTime % daySeconds}
-            onComplete={(totalElapsedTime) => [
-              remainingTime - totalElapsedTime > hourSeconds
-            ]}
-          >
-            {({ elapsedTime }) =>
-              renderTime("hours", getTimeHours(daySeconds - elapsedTime))
-            }
-          </CountdownCircleTimer>
-        </div>
-        <div className="counter">
-          <CountdownCircleTimer
-            {...timerProps}
-            colors={[['#FFF5EE']]}
-            duration={hourSeconds}
-            initialRemainingTime={remainingTime % hourSeconds}
-            onComplete={(totalElapsedTime) => [
-              remainingTime - totalElapsedTime > minuteSeconds
-            ]}
-          >
-            {({ elapsedTime }) =>
-              renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))
-            }
-          </CountdownCircleTimer>
-        </div>
-        <div className="counter">
-          <CountdownCircleTimer
-            {...timerProps}
-            colors={[
-              ['#FFF5EE']
-            ]}
-            duration={minuteSeconds}
-            initialRemainingTime={remainingTime % minuteSeconds}
-            onComplete={(totalElapsedTime) => [
-              remainingTime - totalElapsedTime > 0
-            ]}
-          >
-            {({ elapsedTime }) =>
-              renderTime("seconds", getTimeSeconds(elapsedTime))
-            }
-          </CountdownCircleTimer>
-        </div>
-      </div>
+                duration={daySeconds}
+                initialRemainingTime={remainingTime % daySeconds}
+                onComplete={(totalElapsedTime) => [
+                  remainingTime - totalElapsedTime > hourSeconds
+                ]}
+              >
+                {({ elapsedTime }) =>
+                  renderTime("hours", getTimeHours(daySeconds - elapsedTime))
+                }
+              </CountdownCircleTimer>
+            </div>
+            <div className="counter">
+              <CountdownCircleTimer
+                {...timerProps}
+                colors={[['#FFF5EE']]}
+                duration={hourSeconds}
+                initialRemainingTime={remainingTime % hourSeconds}
+                onComplete={(totalElapsedTime) => [
+                  remainingTime - totalElapsedTime > minuteSeconds
+                ]}
+              >
+                {({ elapsedTime }) =>
+                  renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))
+                }
+              </CountdownCircleTimer>
+            </div>
+            <div className="counter">
+              <CountdownCircleTimer
+                {...timerProps}
+                colors={[
+                  ['#FFF5EE']
+                ]}
+                duration={minuteSeconds}
+                initialRemainingTime={remainingTime % minuteSeconds}
+                onComplete={(totalElapsedTime) => [
+                  remainingTime - totalElapsedTime > 0
+                ]}
+              >
+                {({ elapsedTime }) =>
+                  renderTime("seconds", getTimeSeconds(elapsedTime))
+                }
+              </CountdownCircleTimer>
+            </div>
+          </div> : <h1 className="text-center text-white fst-italic"><mark className="text-dark">TED<sup>x</sup>NITDurgapur is Live</mark></h1>
+      }
     </>
   );
 }
