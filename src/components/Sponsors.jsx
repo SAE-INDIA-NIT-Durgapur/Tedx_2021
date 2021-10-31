e4dce46bb033a287abadf78f429c8a7bfbc074a2
Techno-Simpola <ashukumarsinghrathore123@gmail.com>
2021-10-31 13:40:22 +0530

    almost-F-F

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "../CSS/sponsor.css";
-import img1 from "../images/logo512.png";
-import logo from "./tedxnitdgp.png";
+import  AsnaGarments from "../images/sponsors/1.jpg"
+import GFG from "../images/sponsors/2.jpg"
+import D2C from "../images/sponsors/3.jpg"
+import CTP from "../images/sponsors/4.jpg"
+import ELM from "../images/sponsors/5.jpg"
+import CD from "../images/sponsors/6.jpg"
+import NB from "../images/sponsors/7.jpg"
+import TB from "../images/sponsors/8.jpg"
+import SAE from "../images/sponsors/9.png"
 
 const Sponsors = () => {
   return (
-    <div className="container sponsors">
+    <>
+    {/* <div className="container sponsors">
       <hr />
       <div className="heading-sponsors">
         Why partner with
@@ -14,11 +22,10 @@ const Sponsors = () => {
           TED<sup>x</sup>
           <pull>NITDurgapur?</pull>
         </span>
-        {/* <img className="w-50" src={logo} style={{ height: "25%", width: "25%", marginLeft: "20px!important" }} alt="img" /> */}
       </div>
-      <div className="content-sponsors">
-        <p>
-          TED<sup>x</sup>NITDurgapur being a prolific coalition, has a promising
+       <div className="content-sponsors">
+       <p>
+          TED<sup>x</sup>NITDurgapur being a prolific coalition, has a //promising
           range of audiences from the domains of technology, innovation,
           research, sociology and humanities. This collaboration will have a
           vital impact on innumerable masses through its brand value and rage.
@@ -43,73 +50,80 @@ const Sponsors = () => {
           </div>
         </div>
       </div>
+    </div> */}
+
+    <div className="sponsors">
+        <div className="container container-sponsors text-center">
+            <div className="row row-sponsors">
+                <div className="col-md-12 col-sm-12" text-align="center">
+                    <h1 style={{ color: 'red' }}>Merchandise Partner</h1><br />
+                    <div className="column-sponsors">
+                        <img src={ AsnaGarments } classname="sponsors-img" alt="first" />.
+                        {/* <img src={img1} classname="sponsors-img" alt="first" />
+                        <img src={img1} classname="sponsors-img" alt="first" />
+                        <img src={img1} classname="sponsors-img" alt="first" />
+                        <img src={img1} classname="sponsors-img" alt="first" />
+                        <img src={img1} classname="sponsors-img" alt="first" /> */}
+
+                    </div>
+                    <br/>
+                    <h1 style={{ color: 'red' }}>Associate Sponsor</h1><br />
+                    <div className="column-sponsors">
+                         <img src={GFG} classname="sponsors-img" alt="second" />
+                        {/*<img src={img1} classname="sponsors-img" alt="second2" />
+                        <img src={img1} classname="sponsors-img" alt="second3" /> */}
+                    </div>
+                    <br/>
+                    <h1 style={{ color: 'red' }}>Platform Partner</h1><br />
+                    <div className="column-sponsors">
+                         <img src={D2C} classname="sponsors-img" alt="second" />
+                        {/*<img src={img1} classname="sponsors-img" alt="second" />
+                        <img src={img1} classname="sponsors-img" alt="second" />
+                        <img src={img1} classname="sponsors-img" alt="second" />
+                        <img src={img1} classname="sponsors-img" alt="second" /> */}
+                    </div>
+                    <br/>
+                    <h1 style={{ color: 'red' }}>Education Partner</h1><br />
+                    <div className="column-sponsors">
+                         <img src={ELM} classname="sponsors-img" alt="second" />
+                        {/*<img src={img1} classname="sponsors-img" alt="second2" />
+                        <img src={img1} classname="sponsors-img" alt="second3" /> */}
+                    </div>
+                    <br/>
+                    <h1 style={{ color: 'red' }}>Media Partner</h1><br />
+                    <div className="column-sponsors">
+                         <img src={CTP} classname="sponsors-img" alt="second" />
+                        <img src={CD} classname="sponsors-img" alt="second2" />
+                        <img src={NB} classname="sponsors-img" alt="second3" />
+                        {/*<img src={img1} classname="sponsors-img" alt="second" />
+                        <img src={img1} classname="sponsors-img" alt="second2" />
+                        <img src={img1} classname="sponsors-img" alt="second3" /> */}
+                    </div>
+                    <br/>
+                    <h1 style={{ color: 'red' }}>Learning Partner</h1><br />
+                    <div className="column-sponsors">
+                         <img src={TB} classname="sponsors-img" alt="second" />
+                        {/*<img src={img1} classname="sponsors-img" alt="second2" />
+                        <img src={img1} classname="sponsors-img" alt="second3" />
+                        <img src={img1} classname="sponsors-img" alt="second" />
+                        <img src={img1} classname="sponsors-img" alt="second2" />
+                        <img src={img1} classname="sponsors-img" alt="second3" /> */}
+                    </div>
+                    <br/>
+                    <h1 style={{ color: 'red' }}>Associate Partner</h1><br />
+                    <div className="column-sponsors">
+                        <img src={SAE} classname="sponsors-img" alt="second" />
+                        {/* <img src={img1} classname="sponsors-img" alt="second2" />
+                        <img src={img1} classname="sponsors-img" alt="second3" />
+                        <img src={img1} classname="sponsors-img" alt="second" />
+                        <img src={img1} classname="sponsors-img" alt="second2" />
+                        <img src={img1} classname="sponsors-img" alt="second3" /> */}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    // <div className="sponsors">
-    //     <div className="container container-sponsors text-center">
-    //         <div className="row row-sponsors">
-    //             <div className="col-md-12 col-sm-12" text-align="center">
-    //                 <h1 style={{ color: 'red' }}>Gift Partner</h1><br />
-    //                 <div className="column-sponsors">
-    //                     <img src={img1} classname="sponsors-img" alt="first" />.
-    //                     <img src={img1} classname="sponsors-img" alt="first" />
-    //                     <img src={img1} classname="sponsors-img" alt="first" />
-    //                     <img src={img1} classname="sponsors-img" alt="first" />
-    //                     <img src={img1} classname="sponsors-img" alt="first" />
-    //                     <img src={img1} classname="sponsors-img" alt="first" />
-
-    //                 </div>
-    //                 <h1 style={{ color: 'red' }}> Self Care Partner</h1><br />
-    //                 <div className="column-sponsors">
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second2" />
-    //                     <img src={img1} classname="sponsors-img" alt="second3" />
-    //                 </div>
-    //                 <h1 style={{ color: 'red' }}>Insurance Partner</h1><br />
-    //                 <div className="column-sponsors">
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                 </div>
-    //                 <h1 style={{ color: 'red' }}>Friend</h1><br />
-    //                 <div className="column-sponsors">
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second2" />
-    //                     <img src={img1} classname="sponsors-img" alt="second3" />
-    //                 </div>
-    //                 <h1 style={{ color: 'red' }}>Promoter</h1><br />
-    //                 <div className="column-sponsors">
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second2" />
-    //                     <img src={img1} classname="sponsors-img" alt="second3" />
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second2" />
-    //                     <img src={img1} classname="sponsors-img" alt="second3" />
-    //                 </div>
-    //                 <h1 style={{ color: 'red' }}>Digital Media Partner</h1><br />
-    //                 <div className="column-sponsors">
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second2" />
-    //                     <img src={img1} classname="sponsors-img" alt="second3" />
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second2" />
-    //                     <img src={img1} classname="sponsors-img" alt="second3" />
-    //                 </div>
-    //                 <h1 style={{ color: 'red' }}>Radio Partner</h1><br />
-    //                 <div className="column-sponsors">
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second2" />
-    //                     <img src={img1} classname="sponsors-img" alt="second3" />
-    //                     <img src={img1} classname="sponsors-img" alt="second" />
-    //                     <img src={img1} classname="sponsors-img" alt="second2" />
-    //                     <img src={img1} classname="sponsors-img" alt="second3" />
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // </div>
+    </>
   );
 };
 
